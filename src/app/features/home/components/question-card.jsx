2c0e1b94ed01afe9bec3e8fd87ec14e439bd4b85
PhyoHKo <phyoheinko.phk1@gmail.com>
2026-01-16 22:02:25 +0630

    Remove react-icons and use only the lucide

--- src/app/features/home/components/question-card.jsx
+++ src/app/features/home/components/question-card.jsx
@@ -1,27 +1,26 @@
-import { FaEdit } from "react-icons/fa";
-import { MdModeEdit } from "react-icons/md";
+import { PencilIcon } from "lucide-react";
+import { Edit2Icon } from "lucide-react";
+import { Edit } from "lucide-react";
 
+const QuestionCard = ({ id, title }) => {
+  return (
+    <>
+      <div className="w-auto h-auto p-4 bg-secondary-bg border border-neutral-700 rounded-md cursor-pointer hover:brightness-80 transition-all duration-200">
+        <div className="flex justify-between">
+          <h1 className="font-noto-sans-jp font-medium">問題 {id}</h1>
 
-const QuestionCard = ({id, title}) => {
-    return (
-        <>
-            <div className="w-auto h-auto p-4 bg-secondary-bg border-1 border-neutral-700 rounded-md cursor-pointer hover:brightness-80 transition-all duration-200">
-                <div className="flex justify-between">
-                    <h1 className="font-noto-sans-jp font-medium">問題 {id}</h1>
+          <div className="bg-primary w-9 h-9 rounded-full cursor-pointer flex items-center justify-center">
+            <PencilIcon className="text-white fill-black" />
+          </div>
+        </div>
 
-                    <div className="bg-primary w-9 h-9 rounded-full cursor-pointer flex items-center justify-center">
-                        <MdModeEdit className="text-black"/>
-                    </div>
-                </div>
-
-                {/* Paragraph d mhr */}
-                <div className="max-w-58 h-20 text-xs text-muted-font font-noto-sans-jp bg-transparent pt-3 overflow-hidden">
-                    {title}.....
-                </div>
-
-            </div>
-        </>
-    )
-}
+        {/* Paragraph d mhr */}
+        <div className="max-w-58 h-20 text-xs text-muted-font font-noto-sans-jp bg-transparent pt-3 overflow-hidden">
+          {title}.....
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default QuestionCard;
