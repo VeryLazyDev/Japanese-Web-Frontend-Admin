d490fd66a4524f6f7b9009d90cfdf3561e46d10e
PhyoHKo <phyoheinko.phk1@gmail.com>
2026-01-19 21:18:41 +0630

    Update

---
+++ src/app/features/new_excercise/page.jsx
@@ -0,0 +1,5 @@
+const NewExercisePage = () => {
+  return <div>This is the page for new exercise creation</div>;
+};
+
+export default NewExercisePage;
--- src/app/routes/user-routes.jsx
+++ src/app/routes/user-routes.jsx
@@ -1,10 +1,15 @@
 import HomePageUser from "../features/home-user/page";
+import NewExercisePage from "../features/new_excercise/page";
 
 /** @type {import('react-router-dom').RouteObject[]} */
 const UserRoutes = [
-    {
-        index: true,
-        element: <HomePageUser />,
-    },
+  {
+    index: true,
+    element: <HomePageUser />,
+  },
+  {
+    path: "/reading-module",
+    element: <NewExercisePage />,
+  },
 ];
 export default UserRoutes;
