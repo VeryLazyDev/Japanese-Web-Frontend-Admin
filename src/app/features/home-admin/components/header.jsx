0f8d3333615323325b3c16f7bd97bd326b5b0764
PhyoHKo <phyoheinko.phk1@gmail.com>
2026-01-21 22:28:52 +0630

    Aux Mono font added you can call in className as 'font-aux-mono'

--- src/app/features/home-admin/components/header.jsx
+++ src/app/features/home-admin/components/header.jsx
@@ -1,9 +1,13 @@
 const HomePageHeader = () => {
-    return (
-        <div>
-            <h2 className="text-primary-font font-bold text-2xl mb-1">Reading Management</h2>
-            <h4 className="text-muted-font text-xs">Create and manage reading exercises</h4>
-        </div>
-    );
+  return (
+    <div>
+      <h2 className="text-primary-font font-bold text-2xl mb-1">
+        Reading Management
+      </h2>
+      <h4 className="text-muted-font text-xs">
+        Create and manage reading exercises
+      </h4>
+    </div>
+  );
 };
 export default HomePageHeader;
