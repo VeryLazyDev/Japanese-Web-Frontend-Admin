6356d7eba6cfd4715e385cfa17a895b0e11c27ac
jerry <km2965809@gmail.com>
2026-01-25 21:50:35 +0630

    Create question

--- src/app/routes/admin-routes.jsx
+++ src/app/routes/admin-routes.jsx
@@ -1,11 +1,16 @@
 import HomePageAdminWrapper from "../features/home-admin/wrapper";
 import LoginAdminPage from "../features/login-admin/page";
+import NewExercisePage from "../features/new-excercise/page";
 
 /** @type {import('react-router-dom').RouteObject[]} */
 const AdminRoutes = [
   {
     index: true,
     element: <HomePageAdminWrapper />,
+  },
+  {
+    path: "new-exercise",
+    element: <NewExercisePage />,
   },
 ];
 
