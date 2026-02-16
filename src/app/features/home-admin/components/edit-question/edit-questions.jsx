2a7c3ababbdfcfd8353590eba2dc003d79a26f33
phy0hk <phyoheinko.phk1@gmail.com>
2026-02-16 20:49:00 +0630

    GG

--- src/app/features/home-admin/components/edit-question/edit-questions.jsx
+++ src/app/features/home-admin/components/edit-question/edit-questions.jsx
@@ -2,23 +2,26 @@ import { useEditQuestion } from "../../hooks/useEditQuestions";
 import EditQuestionSection from "./edit-questions-section";
 
 const EditQuestions = () => {
-  const { editQuestionSidebar, closeEdit } = useEditQuestion();
-  return (
-    <>
-      {editQuestionSidebar && (
-        <div
-          onClick={closeEdit}
-          className={`fixed inset-0 bg-black/30 z-40`}
-        ></div>
-      )}
-      <div
-        className={`fixed top-0 right-0 z-50 h-screen transition-all duration-200
-          overflow-hidden p-5 w-120 max-w-full ${editQuestionSidebar ? "translate-x-0" : "translate-x-full"}
-              `}
-      >
-        <EditQuestionSection />
-      </div>
-    </>
-  );
+    const { editQuestionSidebar, closeEdit } = useEditQuestion();
+    return (
+        <>
+            <div
+                className={`absolute inset-0 z-40 flex transition-all duration-200  ${editQuestionSidebar ? "bg-black/30 backdrop-blur-xs" : "pointer-events-none"}`}
+            >
+                <div
+                    className="w-full h-full absolute"
+                    onClick={closeEdit}
+                ></div>
+                <div
+                    className={`top-0 right-0 z-50 h-screen transition-all duration-200 absolute
+                        overflow-hidden p-5 min-w-120 max-w-full ${editQuestionSidebar ? "translate-x-0 " : "translate-x-full"}
+                            `}
+                >
+                    <EditQuestionSection />
+                </div>
+            </div>
+            )
+        </>
+    );
 };
 export default EditQuestions;
--- src/components/dock/dock.jsx
+++ src/components/dock/dock.jsx
@@ -8,63 +8,67 @@ import { Moon } from "lucide-react";
 import useAnimation from "@/hooks/useAnimation";
 
 const Dock = () => {
-  const [currentFocus, setCurrentFocus] = useState(1);
-  const location = useLocation();
-  const { theme, SwitchTheme } = useTheme();
-  const { switchThemeAnimation } = useAnimation();
-  useEffect(() => {
-    switchThemeAnimation(theme);
-    const updateCurrentFocus = () => {
-      const Index = DockPages.findIndex(
-        (item) =>
-          item.route === location.pathname ||
-          item.route + "/" === location.pathname,
-      );
-      setCurrentFocus(Index + 1);
-    };
-    updateCurrentFocus();
-    // switch (location.pathname) {
-    //   case "/admin/login":
-    //     updateCurrentFocus(2);
-    //     break;
-    //   case "/admin":
-    //     updateCurrentFocus(1);
-    //     break;
-    //   case "/admin/settings":
-    //     updateCurrentFocus(3);
-    //     break;
-    //   default:
-    //     updateCurrentFocus(1);
-    // }
-  }, [location, switchThemeAnimation, theme]);
+    const [currentFocus, setCurrentFocus] = useState(1);
+    const location = useLocation();
+    const { theme, SwitchTheme } = useTheme();
+    const { switchThemeAnimation } = useAnimation();
+    useEffect(() => {
+        switchThemeAnimation(theme);
+        const updateCurrentFocus = () => {
+            const Index = DockPages.findIndex(
+                (item) =>
+                    item.route === location.pathname ||
+                    item.route + "/" === location.pathname,
+            );
+            if (Index === -1) {
+                setCurrentFocus(1);
+            } else {
+                setCurrentFocus(Index + 1);
+            }
+        };
+        updateCurrentFocus();
+        // switch (location.pathname) {
+        //   case "/admin/login":
+        //     updateCurrentFocus(2);
+        //     break;
+        //   case "/admin":
+        //     updateCurrentFocus(1);
+        //     break;
+        //   case "/admin/settings":
+        //     updateCurrentFocus(3);
+        //     break;
+        //   default:
+        //     updateCurrentFocus(1);
+        // }
+    }, [location, switchThemeAnimation, theme]);
 
-  return (
-    <div className="p-1 w-fit h-fit border-light-bg border rounded-md gap-1 fixed bottom-3/6 left-5 max-md:hidden z-100 bg-primary-bg shadow">
-      <div
-        className={`bg-primary/80 calc() w-11 h-11 absolute rounded z-0 transition-all`}
-        style={{
-          transform: `translateY(calc(var(--spacing) * 11 * ${currentFocus - 1}))`,
-        }}
-      />
-      {DockPages.map((item) => (
-        <DockBtn
-          pageName={item.pageName}
-          route={item.route}
-          currentFocus={currentFocus}
-          key={item.id}
-          icon={item.icon}
-          id={item.id}
-        />
-      ))}
-      {/* <MdOutlineLightbulb />*/}
-      <button
-        onClick={SwitchTheme}
-        className={`w-11 h-11 flex items-center justify-center bg-neutral-800 rounded md:hidden`}
-      >
-        <Sun className={`absolute light-switch text-neutral-200`} />
-        <Moon className={`absolute dark-switch text-neutral-200`} />
-      </button>
-    </div>
-  );
+    return (
+        <div className="p-1 w-fit h-fit border-light-bg border rounded-md gap-1 fixed bottom-3/6 left-5 max-md:hidden z-100 bg-primary-bg shadow">
+            <div
+                className={`bg-primary/80 calc() w-11 h-11 absolute rounded z-0 transition-all`}
+                style={{
+                    transform: `translateY(calc(var(--spacing) * 11 * ${currentFocus - 1}))`,
+                }}
+            />
+            {DockPages.map((item) => (
+                <DockBtn
+                    pageName={item.pageName}
+                    route={item.route}
+                    currentFocus={currentFocus}
+                    key={item.id}
+                    icon={item.icon}
+                    id={item.id}
+                />
+            ))}
+            {/* <MdOutlineLightbulb />*/}
+            <button
+                onClick={SwitchTheme}
+                className={`w-11 h-11 flex items-center justify-center bg-neutral-800 rounded md:hidden`}
+            >
+                <Sun className={`absolute light-switch text-neutral-200`} />
+                <Moon className={`absolute dark-switch text-neutral-200`} />
+            </button>
+        </div>
+    );
 };
 export default Dock;
